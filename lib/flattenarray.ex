3a35ea3b8f32574dde49295c441948a69719e33a
Herbew <herbew@gmail.com>
2022-04-28 21:03:32 +0700

    change to recursive

--- lib/flattenarray.ex
+++ lib/flattenarray.ex
@@ -1,11 +1,11 @@
 defmodule FlattenArrayRec do
   @doc """
-    Input a list and return the list flattened without nil values.
+    Input a list and return the list flatten_arrayed without nil values.
   """
   @spec flatten_array(list) :: list
-  def flatten_array(list) do
-    list
-    |> List.flatten
-    |> Enum.reject(&(nil == &1))
-  end
+  def flatten_array([]), do: []
+  def flatten_array([[]]), do: []
+  def flatten_array([head | tail]) when is_nil(head), do: flatten_array(tail)
+  def flatten_array([head | tail]) when is_list(head), do: flatten_array(head) ++ flatten_array(tail)
+  def flatten_array([head | tail]), do: [head | flatten_array(tail)]
 end
